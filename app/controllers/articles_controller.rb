6d8f8adf23fff0a2c038500ff3bfa96f87c2bfa0
Michael Dorrance <dorrancemd@gmail.com>
2015-08-04 09:52:08 -0600

    removes database query to the controller from the view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @articles = Article.current
+  end
+
   def show
     @comment = @article.comments.new
   end
